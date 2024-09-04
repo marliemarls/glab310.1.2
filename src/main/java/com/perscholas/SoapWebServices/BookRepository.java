a76b4aba9f83c1f90d36a675bd13d3c70e1cc9ec
Marlie Martinez <marlieahue@gmail.com>
2024-09-04 09:49:45 -0400

    first and final commit

---
+++ src/main/java/com/perscholas/SoapWebServices/BookRepository.java
@@ -0,0 +1,45 @@
+package com.perscholas.SoapWebServices;
+import com.perscholas.xml.book.Book;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class BookRepository {
+    private static final Map<Integer, Book> books = new HashMap<>();
+
+    @PostConstruct
+    public void initData(){
+        Book javaBook = new Book();
+        javaBook.setId(1);
+        javaBook.setTitle("Head first Java");
+        javaBook.setPages(400);
+        books.put(javaBook.getId(), javaBook);
+
+        Book springBook = new Book();
+        springBook.setId(2);
+        springBook.setTitle("Spring in action");
+        springBook.setPages(400);
+        books.put(springBook.getId(), springBook);
+
+        Book pythonBook = new Book();
+        pythonBook.setId(3);
+        pythonBook.setTitle("Learning Python");
+        springBook.setPages(400);
+        books.put(pythonBook.getId(), pythonBook);
+
+        Book hibernateBook = new Book();
+        hibernateBook.setId(4);
+        hibernateBook.setTitle("Hibernate in Action");
+        springBook.setPages(400);
+        books.put(hibernateBook.getId(), hibernateBook);
+    }
+
+    public Book findBookById(int id){
+        Assert.notNull(id, "The book's name must not be null");
+        return books.get(id);
+    }
+
+}
